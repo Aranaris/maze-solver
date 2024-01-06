db71bd1fa09d198de8ed46535ce199712358443c
vinceye <vinceye.cm@gmail.com>
2024-01-06 12:32:21 -0600

    added draw cell class

--- maze.py
+++ maze.py
@@ -6,7 +6,7 @@ class Window:
 		self.height = height
 		self.__root = Tk()
 		self.__root.title = 'test'
-		self.canvas = Canvas(self.__root)
+		self.canvas = Canvas(self.__root, {'bg':'white'})
 		self.canvas.pack()
 		self.running = False
 		self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -44,12 +44,44 @@ class Line:
 		)
 		canvas.pack()
 
+class Cell:
+	def __init__(self, top_left, bottom_right, win):
+		self.has_left_wall = True
+		self.has_right_wall = True
+		self.has_top_wall = True
+		self.has_bottom_wall = True
+		self._top_left = top_left
+		self._top_right = Point(bottom_right.x, top_left.y)
+		self._bottom_left = Point(top_left.x, bottom_right.y)
+		self._bottom_right = bottom_right
+		self._win = win
+
+	def draw(self):
+		walls = []
+		if self.has_left_wall:
+			walls.append(Line(self._top_left, self._bottom_left))
+		if self.has_right_wall:
+			walls.append(Line(self._top_right, self._bottom_right))
+		if self.has_top_wall:
+			walls.append(Line(self._top_left, self._top_right))
+		if self.has_bottom_wall:
+			walls.append(Line(self._bottom_left, self._bottom_right))
+		for wall in walls:
+			self._win.draw_line(wall, 'black')
+
+
 def main():
 	win = Window(800, 600)
 	point1 = Point(0, 0)
 	point2 = Point(100, 100)
-	line = Line(point1, point2)
-	win.draw_line(line, 'red')
+	point3 = Point(200, 200)
+	cell1 = Cell(point2, point3, win)
+	cell2 = Cell(point1, point2, win)
+	cell1.has_left_wall = False
+	
+	cell1.draw()
+	cell2.draw()
+
 	win.wait_for_close()
 
 	
